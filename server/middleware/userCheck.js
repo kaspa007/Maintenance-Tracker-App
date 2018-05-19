82cfd6a3ffe8ebfcc42ae87a31785c3ad1201629
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2018-05-20 00:29:53 +0100

    [157718171] signin API endpoints complete

---
+++ server/middleware/userCheck.js
@@ -0,0 +1,22 @@
+class UserValidate {
+    static loginValidator(req, res, next) {
+        // if (req.body.password.length < 4) {
+        //     return res.status(400).json({
+        //         message: 'Password must be more than 4 characters',
+        //         error: true
+        //     })
+        // }
+        req.check('email', 'Email is not valid').isEmail();
+        req.check('password', 'Password is less than 5 characters').isLength({min:5})
+
+        const errors = req.validationErrors();
+        if (errors) {
+            return res.status(400).json({
+                error: errors[0].msg
+            });
+        }
+        next();
+    }
+}
+
+export default UserValidate;
