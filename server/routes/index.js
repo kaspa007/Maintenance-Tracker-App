86f2eb60c549d865acc03887d325e57e97a2876c
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2018-05-20 09:39:16 +0100

    [finished 177719818] created GET /users API endpoint to get all users

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,8 +7,10 @@ const router = express.Router();
 const { UserSignIn } = Users;
 const { loginValidator } = UserValidate;
 const { UserSignUp } = Users;
+const { AllUsers } = Users;
 
 router.post('/users/signin', loginValidator, UserSignIn);
 router.post('/users/signup', UserSignUp);
+router.get('/users', AllUsers);
 
 export default router;
