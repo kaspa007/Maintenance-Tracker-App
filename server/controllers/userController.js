f216347f2b9c497ce764ab5435d578cd240bc14f
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2018-05-20 09:24:08 +0100

    [157718579] SIgnup API created as POST /users/signup

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,7 @@ class Users {
     static UserSignIn(req, res) {
         const { email, password } = req.body;
         for (let i = 0; i < users.length; i++) {
-            if (email.toLowerCase() == users[i].email && password == users[i].password) {
+            if (email.toLowerCase() == users[i].email.toLowerCase() && password == users[i].password) {
             return res.status(200).json({
                 message: 'Logged in successfully!',
                 error: false
@@ -21,6 +21,22 @@ class Users {
             message: 'Error logging in',
             error: true
         })
+    }
+
+  
+    static UserSignUp(req, res) {
+        const { name, password, email } = req.body;
+        users.push ({
+            id: users.length + 1,
+            name, 
+            password, 
+            email
+        });
+        // IdGen += 1;
+        return res.status(200).json({
+            message: 'Signup succesful!',
+            error: false
+        });
     }
 }
 export default Users;
