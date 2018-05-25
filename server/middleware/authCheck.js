21eff980863e035a7d54036fa4feb7c8f8d522da
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2018-05-25 15:13:25 +0100

    Changes on Create request API

--- server/middleware/authCheck.js
+++ server/middleware/authCheck.js
@@ -4,6 +4,7 @@ import users from '../models/users';
 class Authenticator {
     static userAuth ( req, res, next) {
         const token = req.headers['x-access-token'];
+        console.log("help me");
         const decoded = jwt.verify( token, "secret" ); {
              if (!decoded) {
                     return res.status(400).json({
@@ -16,6 +17,7 @@ class Authenticator {
     }
     static reqAuth ( req, res, next) {
         const { subject, requestType, requestDetails } = req.body;
+        console.log("help me");
 
        if (!( subject.trim() || requestType.trim() || requestDetails.trim())) {
         return res.status(400).json({
